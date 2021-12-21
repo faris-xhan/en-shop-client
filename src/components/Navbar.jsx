9203eb77f54fd552567c5634abd2bd278a43d183
faris <faris@mail.com>
2021-12-21 16:46:46 +0500

    :sparkles: add searchbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import BootstrapNavbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { Brand } from './Brand';
+import Searchbar from './Searchbar';
 
 const Navbar = (props) => {
   return (
     <BootstrapNavbar bg="dark" variant="dark">
       <Container>
         <Brand />
+        <Searchbar />
       </Container>
     </BootstrapNavbar>
   );
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+
+const StyledSearchbar = styled(FormControl)`
+  width: 230px;
+  opacity: 0.97;
+  transition: opacity 260ms ease-in-out;
+  &:focus {
+    opacity: 1;
+  }
+`;
+
+const Searchbar = (props) => {
+  const [input, setInput] = useState('');
+  const handleInputChange = (event) => setInput(event.target.value);
+
+  return (
+    <Form className="d-flex">
+      <StyledSearchbar
+        size="sm"
+        type="search"
+        className="me-2"
+        placeholder="Search"
+        aria-label="Search"
+        value={input}
+        onChange={handleInputChange}
+      />
+    </Form>
+  );
+};
+
+export default Searchbar;
