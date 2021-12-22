37772765a16221456d5fa92364580bfd9b6bb6d9
faris <faris@mail.com>
2021-12-22 13:55:57 +0500

    :art: update login page styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,25 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Alert } from 'react-bootstrap';
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const BackgroundContainer = styled(Container)`
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url('https://www.netsafe.org.nz/wp-content/uploads/2019/11/Online-Shopping_WebHeader_1141x641.jpg')
+      center;
+  background-size: cover;
+`;
+
+const LoginButton = styled(Button)`
+  width: 200px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+  text-transform: uppercase;
+`;
 
 const Login = (props) => {
   const [error, setError] = useState('');
@@ -25,8 +44,17 @@ const Login = (props) => {
     }
   };
   return (
-    <Container className="d-flex align-items-center justify-content-center h-100 bg-transparent">
-      <Container className="bg-transparent">
+    <BackgroundContainer
+      fluid
+      className="d-flex align-items-center justify-content-center h-100 bg-transparent"
+    >
+      <Container
+        className="p-5 rounded rounded-5"
+        style={{
+          maxWidth: '600px',
+          backgroundColor: 'white',
+        }}
+      >
         <h1 className="text-center display-1 fw-bold mb-2">Login</h1>
         {error && (
           <Alert variant="danger" onClose={() => setError('')} dismissible>
@@ -55,12 +83,14 @@ const Login = (props) => {
           <Form.Group className="mb-3" controlId="remember">
             <Form.Check type="checkbox" name="remember" label="Remember" />
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
+          <div className="d-flex justify-content-center align-items-center">
+            <LoginButton variant="primary" type="submit" size="lg">
+              Login
+            </LoginButton>
+          </div>
         </Form>
       </Container>
-    </Container>
+    </BackgroundContainer>
   );
 };
 
